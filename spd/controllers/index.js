07955f66de77edb2c8bb190a8212c7363bad7ed4
Brandon Kelley <14bmkelley@gmail.com>
2015-06-03 11:48:10 -0700

    switching to ejs

---
+++ spd/controllers/index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+
+var FullQuerys = require("../models/membership");
+
+/* GET home page */
+router.get("/", function(req, res, next) {
+  res.render("home", {
+    title: "Home",
+    error: null
+  });
+});
+
+/* GET about us page */
+router.get("/about_us", function(request, response) {
+  FullQuerys.queryPledgeClasses(function(data) {
+    response.render("about_us", {
+      title: "About Us",
+      error: "null",
+    });
+  });
+});
+
+/* GET recruitment page */
+router.get("/recruitment", function(request, response) {
+  response.render("recruitment", {
+    title: "Recruitment",
+    error: null
+  });
+});
+
+/* GET contact us page */
+router.get("/contact_us", function(request, response) {
+  response.render("contact_us", {
+    title: "About Us",
+    error: null
+  });
+});
+
+/* GET profile page */
+router.get("/internal", function(request, response) {
+  response.render("external", {
+    title: "External",
+    error: null
+  });
+});
+
+module.exports = router;
---
+++ spd/models/membership.js
@@ -0,0 +1,100 @@
+// These functions provide an interface to the database
+// that returns the entire membership, sorted in various
+// ways.
+
+var aws = require("aws-sdk");
+var dynamodb = new aws.DynamoDB({ region: "us-west-2" });
+
+exports.queryPledgeClasses = function(callback) {
+  
+  var membership = {};
+  var pledgeClass = [];
+  var classes = [];
+  
+  var scanParams = {
+    TableName: "membership",
+    IndexName: "class",
+    AttributesToGet: ["class"]
+  }
+  
+  //scan the database for names of pledge classes
+  var classScan = function(scanParams, callback) {
+    dynamodb.scan(scanParams, function(error, data) {
+      if (error) {
+        console.log(error);
+      } else {
+        var list = data.Items;
+        var length = data.Count;
+        for (var i = 0; i < length; i++) {
+          var found = false;
+          for (var j = 0; j < pledgeClasses.length; j++) {
+            if (list[i]["class"]["S"] == pledgeClasses[j]) {
+              found = true;
+              break;
+            }
+          }
+          if (!found || i === 0) {
+            pledgeClasses.push(list[i]["class"]["S"]);
+            membership[list[i]["class"]["S"]] = [];
+          }
+          if (i === length - 1) {
+            callback();
+          }
+        }
+      }
+    });
+  };
+  
+  var classQuery = function(pledgeClass, callback) {
+    for (var i = 0; i < pledgeClass.length; i++) {
+      var queryParams = {
+        IndexName: "class",
+        KeyConditions: {
+          "class": {
+            ComparisonOperator: "EQ",
+            AttributeValueList: [{ "S": pledgeClass[i] }]
+          }
+        },
+        TableName: "membership",
+        AttributesToGet: ["firstname", "lastname"]
+      }
+      classes.push({ 
+        pledgeClasses: pledgeClass[i],
+        params: queryParams
+      });
+      if (i === pledgeClasses.length - 1) {
+        callback();
+      }
+    }
+  };
+  
+  var dbQuery = function(classes, pledgeClassesToGo, callback) {
+    var pledgeClassIndex = pledgeClassesToGo - 1;
+    if (pledgeClassIndex >= 0) {
+      dynamodb.query(classes[pledgeClassIndex]["params"], function(error, data) {
+        if (error) {
+          console.log(error);
+        } else {
+          var list = data.Items;
+          var length = data.Count;
+          for (var i = 0; i < length; i++) {
+            var firstname = list[i]["firstname"]["S"];
+            var lastname = list[i]["lastname"]["S"];
+            membership[classes[pledgeClassIndex]["pledgeClass"]].push({ "firstname": firstname, "lastname": lastname });
+          }
+        }
+        dbQuery(classes, pledgeClassesToGo - 1, callback);
+      });
+    } else {
+      callback();
+    }
+  };
+  
+  classScan(scanParams, function() {
+    classQuery(pledgeClasses, function() {
+      dbQuery(classes, classes.length, function() {
+        callback(membership);
+      });
+    });
+  });
+}
