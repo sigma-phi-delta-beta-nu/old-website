99b60feb5d05b68e58aa492ad2943331e031c9d9
Brandon Kelley <webmaster@spdcalpoly.org>
2015-07-20 18:14:37 +0000

    pretty restart.log

--- bin/restart.sh
+++ bin/restart.sh
@@ -2,11 +2,17 @@
 
 if (($(ps -ef | grep "node ./bin/http" | grep -v "sh -c" | grep -v grep | wc -l) > 1))
   then
-    echo "Servers are up.\n"
+    #green text
+    echo -e "\e[32mServers are up."
+    echo
   else
+    #red text
+    echo -e "\e[31mOne of the servers was down... restarting."
     cd ~/website/
     screen -X -S server kill
     screen -X -S secure_server kill
     ./bin/start.sh
-    echo "Servers restarted.\n"
+    #green text
+    echo -e "\e[32mServers restarted."
+    echo
 fi
