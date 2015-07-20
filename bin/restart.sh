82fe18d46434fff0c5c194d2797ac4c5c1f61d04
Brandon Kelley <webmaster@spdcalpoly.org>
2015-07-20 19:02:56 +0000

    hotfixes

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if (($(ps -ef | grep "node ./bin/http" | grep -v "sh -c" | grep -v grep | wc -l) > 1))
+if (($(ps -ef | grep "node ./bin/server/http" | grep -v "sh -c" | grep -v grep | wc -l) > 1))
   then
-    echo -e "\e[0;32mServers are up."
+    echo "Servers are up."
     echo
   else
-    echo -e "\e[0;31mOne of the servers was down... restarting."
+    echo "One of the servers was down... restarting."
     cd ~/website/
     screen -X -S server kill
     screen -X -S secure_server kill
     ./bin/start.sh
-    echo -e "\e[0;32mServers restarted."
+    echo "Servers restarted."
     echo
 fi
