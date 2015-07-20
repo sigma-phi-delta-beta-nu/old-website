0d599c6478ac88fb4c64aab471fe634ce5045549
Brandon Kelley <webmaster@spdcalpoly.org>
2015-07-20 18:29:35 +0000

    still not working....

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+Green = '\e[0;32m'
+Red = '\e[0;31m'
+
 if (($(ps -ef | grep "node ./bin/http" | grep -v "sh -c" | grep -v grep | wc -l) > 1))
   then
-    #green text
-    echo -e "\e[1;32mServers are up."
+    echo -e "${Green}Servers are up."
     echo
   else
-    #red text
-    echo -e "\e[1;31mOne of the servers was down... restarting."
+    echo -e "${Red}One of the servers was down... restarting."
     cd ~/website/
     screen -X -S server kill
     screen -X -S secure_server kill
     ./bin/start.sh
-    #green text
-    echo -e "\e[1;32mServers restarted."
+    echo -e "${Green}Servers restarted."
     echo
 fi
