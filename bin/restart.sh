773cf6e0e81c5f8a43ad613520cd103bd79924cf
Brandon Kelley <webmaster@spdcalpoly.org>
2015-07-20 18:21:35 +0000

    Color not working

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,16 +3,16 @@
 if (($(ps -ef | grep "node ./bin/http" | grep -v "sh -c" | grep -v grep | wc -l) > 1))
   then
     #green text
-    echo -e "\e[32mServers are up."
+    echo -e "\e[1;32mServers are up."
     echo
   else
     #red text
-    echo -e "\e[31mOne of the servers was down... restarting."
+    echo -e "\e[1;31mOne of the servers was down... restarting."
     cd ~/website/
     screen -X -S server kill
     screen -X -S secure_server kill
     ./bin/start.sh
     #green text
-    echo -e "\e[32mServers restarted."
+    echo -e "\e[1;32mServers restarted."
     echo
 fi
