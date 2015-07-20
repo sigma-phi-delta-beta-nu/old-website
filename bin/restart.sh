4db3e19491f7c7791d19a84df031330bf185e550
Brandon Kelley <webmaster@spdcalpoly.org>
2015-07-20 18:44:28 +0000

    think I got it this time

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-Green = '\e[0;32m'
-Red = '\e[0;31m'
-
 if (($(ps -ef | grep "node ./bin/http" | grep -v "sh -c" | grep -v grep | wc -l) > 1))
   then
-    echo -e "${Green}Servers are up."
+    echo -e "\e[0;32mServers are up."
     echo
   else
-    echo -e "${Red}One of the servers was down... restarting."
+    echo -e "\e[0;31mOne of the servers was down... restarting."
     cd ~/website/
     screen -X -S server kill
     screen -X -S secure_server kill
     ./bin/start.sh
-    echo -e "${Green}Servers restarted."
+    echo -e "\e[0;32mServers restarted."
     echo
 fi
