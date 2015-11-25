2ab800df777ec0dba92f59f9be5d764f6ea30b60
Brandon Kelley <webmaster@spdcalpoly.org>
2015-11-25 20:12:26 +0000

    small changes for server

--- server/https.js
+++ server/https.js
@@ -1,11 +1,11 @@
-var app = require("../app");
+var app = require("../app")();
 var https = require("https");
 var fs = require("fs");
 
 var ssl_options = {
-  key: fs.readFileSync(__dirname + "/../keys/server.key"),
-  cert: fs.readFileSync(__dirname + "/../keys/server.crt"),
-  ca: fs.readFileSync(__dirname + "/../keys/server.ca")
+  key: fs.readFileSync(__dirname + "/keys/spdcalpoly.key"),
+  cert: fs.readFileSync(__dirname + "/keys/spdcalpoly.crt"),
+  ca: fs.readFileSync(__dirname + "/keys/spdcalpoly.intermediate.key")
 };
 
 https.createServer(ssl_options, app).listen(9001 || process.argv[2]);
