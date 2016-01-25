1589d07e4e01fbee334a9d6bf5e185186368b213
andrewjpiro <andrewjpiro@gmail.com>
2016-01-24 21:30:21 -0800

    Add back content and links

--- controllers/render.js
+++ controllers/render.js
@@ -1,74 +1,74 @@
 var renderController = function(router, context) {
-  
+
   var auth = context.sessionManager.authenticate;
   var User = context.models.User;
   var Event = context.models.Event;
   var Photo = context.models.Photo;
-  
+
   /* GET home page */
   router.get("/", function(request, response) {
     auth(request.cookies, function(user) {
       response.render("home", {
-        "title": "Home",
+        "page": "content/home",
         "user": user
       });
     });
   });
-  
+
   /* GET about us page */
   router.get("/about_us", function(request, response) {
     auth(request.cookies, function(user) {
       User.getClasses(function(classes) {
-        response.render("about_us", {
-          "title": "About Us",
+        response.render("template", {
+          "page": "content/about_us",
           "user": user,
           "classes": classes
         });
       });
     });
   });
-  
+
   /* GET recruitment page */
   router.get("/recruitment", function(request, response) {
     auth(request.cookies, function(user) {
       response.render("template", {
-        "title": "Recruitment",
+        "page": "content/recruitment",
         "user": user
       });
     });
   });
-  
+
   /* GET events page */
   router.get("/events", function(request, response) {
     auth(request.cookies, function(user) {
       var username = (user) ? user.username : null;
       Event.getCategories(username, function(events) {
         response.render("template", {
-          "title": "Events",
+          "page": "content/events",
           "user": user,
           "events": events
         });
       });
     });
   });
-  
+
   /* GET new event form page */
   router.get("/new_event", function(request, response) {
     auth(request.cookies, function(user) {
       if (user !== null) {
         response.render("template", {
-          "title": "New Event",
+          "page": "content/new_event",
           "user": user
         });
       } else {
         response.render("template", {
-          "title": "Home",
+          "page": "content/home",
           "user": user
         });
       }
     });
   });
-  
+
   /* GET single event page */
   router.get("/events/*", function(request, response) {
     auth(request.cookies, function(user) {
@@ -76,38 +76,38 @@ var renderController = function(router, context) {
       var path = request.path.substring(7, request.path.length);
       Event.get(username, path, function(eventFound) {
         response.render("template", {
-          "title": "Event",
+          "page": "content/event",
           "user": user,
           "event": eventFound
         });
       });
     });
   });
-  
+
   /* GET gallery page */
   router.get("/gallery", function(request, response) {
     auth(request.cookies, function(user) {
       var username = (user) ? user.username : null;
       Photo.getAlbums(username, function(albums) {
         response.render("template", {
-          "title": "Gallery",
+          "page": "content/gallery",
           "user": user,
           "gallery": albums
         });
       });
     });
   });
-  
+
   /* GET new photo page */
   router.get("/new_photo", function(request, response) {
     auth(request.cookies, function(user) {
       response.render("template", {
-        "title": "New Photo",
+        "page": "content/new_photo",
         "user": user
       });
     });
   });
-  
+
   /* GET single gallery album or image page */
   router.get("/gallery/*", function(request, response) {
     auth(request.cookies, function(user) {
@@ -123,7 +123,7 @@ var renderController = function(router, context) {
         // Query a photo album
         Photo.getAlbum(username, path, function(album) {
           response.render("template", {
-            "title": "Album",
+            "page": "content/album",
             "user": user,
             "album": album,
           });
@@ -133,57 +133,57 @@ var renderController = function(router, context) {
         var imagePath = path.substring(imageIndex, path.length);
         Photo.get(username, imagePath, function(photoFound) {
           response.render("template", {
-            "title": "Photo",
+            "page": "content/photo",
             "user": user,
             "photo": photoFound
-          }); 
+          });
         });
       }
     });
   });
-  
+
   /* GET contact us page */
   router.get("/contact_us", function(request, response) {
     auth(request.cookies, function(user) {
       User.getPositions(function(positions) {
         response.render("template", {
-          "title": "Contact Us",
+          "page": "content/contact_us",
           "user": user,
           "positions": positions
         });
       });
     });
   });
-  
+
   /* GET profile page */
   router.get("/dashboard", function(request, response) {
     auth(request.cookies, function(user) {
       if (user === null) {
         response.render("template", {
-          "title": "Home",
+          "page": "content/home",
           "user": user
         });
       } else {
         response.render("template", {
-          "title": "Dashboard",
+          "page": "content/dashboard",
           "user": user
         });
       }
     });
   });
-  
+
   /* GET roster */
   router.get("/roster", function(request, response) {
     auth(request.cookies, function(user) {
       if (user === null) {
         response.render("template", {
-          "title": "Home",
+          "page": "content/home",
           "user": user
         });
       } else {
         User.getAllPrivate(function(roster) {
           response.render("template", {
-            "title": "Roster",
+            "page": "content/roster",
             "user": user,
             "roster": roster
           });
@@ -191,9 +191,9 @@ var renderController = function(router, context) {
       }
     });
   });
-  
+
   return router;
-  
+
 };
 
 module.exports = renderController;
